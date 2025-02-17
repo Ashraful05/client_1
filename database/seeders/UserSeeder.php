6d49ce0352e6c74ed35d9f7c6595c256e773fb93
Ashraful05 <jamalrasu420@gmail.com>
2025-02-17 22:02:30 +0600

    multi_user_role_set_in_user_table_with_fake_data_inserted

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -14,10 +15,32 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-           'name'=>'admin',
-            'email'=>'admin@admin.com',
-            'password'=>Hash::make('12345678')
+        DB::table('users')->insert([
+            //admin
+            [
+                'name'=>'admin',
+                'email'=>'admin@admin.com',
+                'password'=>Hash::make('12345678'),
+                'role'=>'admin',
+                'status'=>'active'
+            ],
+            //user
+            [
+                'name'=>'user_1',
+                'email'=>'user_1@user.com',
+                'password'=>Hash::make('12345678'),
+                'role'=>'user',
+                'status'=>'active'
+            ],
+            //agent
+            [
+                'name'=>'agent_1',
+                'email'=>'agent_1@agent.com',
+                'password'=>Hash::make('12345678'),
+                'role'=>'agent',
+                'status'=>'active'
+            ],
         ]);
+
     }
 }
